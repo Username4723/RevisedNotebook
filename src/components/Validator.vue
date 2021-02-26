5bb94e69f75cd95c5b3b3992849436bba45c2ad1
Username4723 <11415806+Username4723@users.noreply.github.com>
2021-02-25 22:16:42 -0600

    Add some utilities

--- src/components/Validator.vue
+++ src/components/Validator.vue
@@ -7,6 +7,12 @@
           <b-card-header>
             <b-card-title>Validation</b-card-title>
           </b-card-header>
+          <b-card-body>
+            <b-button :pressed.sync="hidePassing" :variant="hidePassing ? 'danger' : 'success' "> {{ hidePassing ? "Show Passing" : "Hide Passing" }} </b-button>
+            Failing Total: {{ failingPages + failingHomebrew }}
+            Failing Pages: {{ failingPages }}
+            Failing Homebrew: {{ failingHomebrew }}
+          </b-card-body>
         </b-card>
       </b-col>
     </b-row>
@@ -21,7 +27,7 @@
           </b-card-header>
           <b-card-body>
             <b-list-group>
-              <b-list-group-item v-for="(item, index) in homebrew" :key="index" :variant="homebrewSchema(item) ? 'success' : 'danger'">
+              <b-list-group-item v-for="(item, index) in (hidePassing ? failingData.homebrew : this.homebrew)" :key="index" :variant="homebrewSchema(item) ? 'success' : 'danger'">
                 {{ item.name ? item.name : "No Name: " + JSON.stringify(item) }}
                 <div v-if="!homebrewSchema(item)">
                   {{ homebrewSchema.errors }}
@@ -36,7 +42,7 @@
 
     <!-- pages -->
     <b-row>
-      <b-col v-for="(group, index) in pages" :key="index">
+      <b-col v-for="(group, index) in (hidePassing ? failingData.pages : pages)" :key="index">
         <b-card>
           <b-card-header>
             <b-card-title>Pages: {{ index }}</b-card-title>
@@ -71,10 +77,36 @@ export default {
         character: require('@/data/character.json'),
         world: require('@/data/world.json'),
       },
-      homebrew: require('@/data/homebrew_links.json')
+      homebrew: require('@/data/homebrew_links.json'),
+      hidePassing: true
     }
   },
   computed: {
+    failingData: function() {
+      const data = {
+        pages: {},
+        homebrew: {}
+      }
+
+      for (let key in this.pages) {
+        data.pages[key] = {};
+        for (let entry in this.pages[key]) {
+          if (!this.pageSchema(this.pages[key][entry])) data.pages[key][entry] = this.pages[key][entry]
+        }
+      }
+
+      for (let key in this.homebrew) {
+        if (!this.homebrewSchema(this.homebrew[key])) data.homebrew[key] = this.homebrew[key]
+      }
+
+      return data;
+    },
+    failingPages: function() {
+      return Object.values(this.failingData.pages).map(it => Object.values(it).length).reduce((value, acc) => acc + value, 0)
+    },
+    failingHomebrew: function() {
+      return Object.values(this.failingData.homebrew).length
+    },
     pageSchema: function() {
       return ajv.compile(require('@/schemas/page.schema.json'))
     },
