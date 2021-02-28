11e25497c7402499cabdda1fde7132d6ea99a9f0
Username4723 <11415806+Username4723@users.noreply.github.com>
2021-02-27 21:34:01 -0600

    Pretty is fun

--- src/components/game/RecursiveNavbar.vue
+++ src/components/game/RecursiveNavbar.vue
@@ -1,40 +1,30 @@
 <template>
-  <div>
+  <b-container>
     <div v-for="(page, key) in pages" :key="key">
       <b-row class="mb-1">
         <div>
           <!-- Button -->
-          <b-button v-if="page.subpages" size="sm" variant="primary" v-b-toggle="key">
-            <BIconArrowBarDown></BIconArrowBarDown>
+          <b-button size="sm"
+            :variant="hasContent(page) ? (isCurrent(key) ? 'success' : 'primary') : 'info'"
+            :to="hasContent(page) ? { name: type, params: { id: getSubkey(key) }} : undefined" v-b-toggle="key">
+            {{ page.title }}
           </b-button>
-          <b-button size="sm" v-else>
-            <BIconDashCircle></BIconDashCircle>
-          </b-button>
-
-          <b-link v-if="page.text || page.images || page.table" :to="{ name: type, params: { id: route == '' ? key : route + '.' + key }}"> {{ page.title }} </b-link>
-          <span v-else> {{ page.title }} </span>
         </div>
       </b-row>
-      <b-row v-if="page.subpages" class="mb-1">
+      <b-row class="mb-1">
         <b-collapse :accordion="'a-' + route" :id="key">
-          <b-card>
-            <RecursiveNavbar :pages="page.subpages" :type="type" :route="route == '' ? key : route + '.' + key"></RecursiveNavbar>
+          <b-card v-if="page.subpages">
+            <RecursiveNavbar :pages="page.subpages" :type="type" :route="getSubkey(key)"></RecursiveNavbar>
           </b-card>
         </b-collapse>
       </b-row>
     </div>
-  </div>
+  </b-container>
 </template>
 
 <script>
-import { BIconArrowBarDown, BIconDashCircle } from 'bootstrap-vue'
-
 export default {
   name: "RecursiveNavbar",
-  components: {
-    BIconArrowBarDown,
-    BIconDashCircle
-  },
   props: {
     pages: {
       type: Object,
@@ -47,6 +37,17 @@ export default {
     route: {
       type: String,
       default: ''
+    }
+  },
+  methods: {
+    hasContent: function(page) {
+      return page.text || page.images || page.table
+    },
+    isCurrent: function(key) {
+      return this.$route.params.id == this.route + '.' + key  || this.$route.params.id == key
+    },
+    getSubkey: function(key) {
+      return this.route == '' ? key : this.route + '.' + key
     }
   }
 }
