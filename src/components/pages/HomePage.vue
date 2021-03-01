31960b968101abd4fdd0fd1824e9b367a88acebb
Username4723 <11415806+Username4723@users.noreply.github.com>
2021-02-28 22:27:30 -0600

    Add Red's GSheet

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -7,6 +7,10 @@
     </b-card-header>
     <b-card-body>
       <p>This is my D&D Notebook. Please don't metagame too badly.</p>
+
+      <p>You're probably looking for this.</p>
+
+      <b-button variant='primary' href='https://docs.google.com/spreadsheets/d/18Z1Yaxod-cU0IprTdyoFPr1RW8p6lYkz1lFBzjshMcM/edit?usp=drive_web&ouid=115142078076961767194'>Red's GSheet</b-button>
     </b-card-body>
   </b-card>
 </template>
